895c7122333ab692b7a2ecbc15af44820dc1eb54
Nexxel <sergiogonzalezmuriel@gmail.com>
2017-04-06 12:00:32 +0200

    Submission
    This is the test that works with just 1 room made of 1 element

---
+++ Gamecontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamecontroller : MonoBehaviour {
+	public int initialI = 0;
+	public int initialJ = 0;
+	public int lastI = 40;
+	public int lastJ = 40;
+    [Range(0, 5)]
+    public int numRooms;
+	public GameObject wall;
+
+	private Rooms[] rooms;
+
+	// Use this for initialization
+	void Start () {
+        rooms = new Rooms[numRooms];
+		for (int i = initialI; i < lastI; i++) {
+			for (int j = initialJ; j < lastJ; j++) {
+				Instantiate (wall,new Vector2 (i,j),Quaternion.identity);
+			}
+		}
+		Vector2 init = new Vector2(Random.Range (initialI+1,lastI-1),Random.Range (initialJ+1,lastJ-1));
+        Rooms newR = GetComponent<Rooms>();
+		newR.createRoom ((int)Random.Range (1,lastI-init.x),(int)Random.Range (1,lastJ-init.y),init);
+        rooms.SetValue(newR, 0);
+	}
+}
---
+++ Rooms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms : MonoBehaviour {
+
+
+	public GameObject free;
+
+    private int _firstI, _firstJ, _lastI, _lastJ;
+
+    public void createRoom (int width, int height, Vector2 first)
+	{
+		_firstI = (int)first.x;
+		_firstJ = (int)first.y;
+		_lastI = _firstI + width;
+		_lastJ = _firstJ + height;
+		for (int i = _firstI; i < _lastI; i++) {
+			for (int j = _firstJ; j < _lastJ; j++) {
+				Instantiate (free,new Vector3 (i,j,-1),Quaternion.identity);
+			}
+		}
+	}
+}
