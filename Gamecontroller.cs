df92500e30ec0cadcd87184107712eb93c1c3b97
Nexxel <sergiogonzalezmuriel@gmail.com>
2017-04-16 19:27:22 +0200

    çupdate of scripts
    Now we can generate more than 1 room without collisions and with more
    than 1 kind of elements inside each room

--- Gamecontroller.cs
+++ Gamecontroller.cs
@@ -7,23 +7,51 @@ public class Gamecontroller : MonoBehaviour {
 	public int initialJ = 0;
 	public int lastI = 40;
 	public int lastJ = 40;
-    [Range(0, 5)]
-    public int numRooms;
+    [Range(1, 10)]
+    public int tries = 1;
+    [Range(1, 20)]
+    public int numRooms = 1;
 	public GameObject wall;
 
+    public static bool[ , ] occupiedPositions; //False -> Not ocuppied
+
+    private GameObject wallController;
 	private Rooms[] rooms;
 
 	// Use this for initialization
 	void Start () {
+        occupiedPositions = new bool[lastI - initialI+1, lastJ - initialJ+1];
+        wallController = new GameObject();
+        wallController.name = "Wall";
         rooms = new Rooms[numRooms];
-		for (int i = initialI; i < lastI; i++) {
-			for (int j = initialJ; j < lastJ; j++) {
-				Instantiate (wall,new Vector2 (i,j),Quaternion.identity);
+        for (int i = initialI; i <= lastI; i++) {
+			for (int j = initialJ; j <= lastJ; j++) {
+                if(i == initialI || i == lastI || j == initialJ || j == lastJ)
+                {
+				    GameObject newWall = Instantiate (wall,new Vector2 (i,j),Quaternion.identity);
+                    newWall.transform.SetParent(wallController.transform);
+                    occupiedPositions[i-initialI,j-initialJ] = true;
+                }
 			}
 		}
-		Vector2 init = new Vector2(Random.Range (initialI+1,lastI-1),Random.Range (initialJ+1,lastJ-1));
-        Rooms newR = GetComponent<Rooms>();
-		newR.createRoom ((int)Random.Range (1,lastI-init.x),(int)Random.Range (1,lastJ-init.y),init);
-        rooms.SetValue(newR, 0);
+        for(int i = 0; i<numRooms; i++)
+        {
+		    Vector2 init = new Vector2(Random.Range (initialI+1,lastI-1),Random.Range (initialJ+1,lastJ-1));
+            Rooms newR = GetComponent<Rooms>();
+		    newR.createRoom ((int)Random.Range (2,lastI-init.x),(int)Random.Range (2,lastJ-init.y),init);
+            int counter = 0;
+            while (newR.getRoomController() == null && counter<tries)
+            {
+                init = new Vector2(Random.Range(initialI + 1, lastI - 1), Random.Range(initialJ + 1, lastJ - 1));
+                newR.createRoom((int)Random.Range(2, lastI - init.x), (int)Random.Range(2, lastJ - init.y), init);
+                counter++;
+            }
+            rooms.SetValue(newR, i);
+        }
 	}
+
+    public Rooms[] getRooms()
+    {
+        return rooms;
+    } 
 }
--- Rooms.cs
+++ Rooms.cs
@@ -5,20 +5,84 @@ using UnityEngine;
 public class Rooms : MonoBehaviour {
 
 
-	public GameObject free;
+	public GameObject[] elements;
 
+    private GameObject roomController;
     private int _firstI, _firstJ, _lastI, _lastJ;
+
+    private bool[,] occupied;
+    private Gamecontroller aux;
 
     public void createRoom (int width, int height, Vector2 first)
 	{
+        occupied = Gamecontroller.occupiedPositions;
+        aux = GetComponent<Gamecontroller>();
 		_firstI = (int)first.x;
 		_firstJ = (int)first.y;
 		_lastI = _firstI + width;
 		_lastJ = _firstJ + height;
-		for (int i = _firstI; i < _lastI; i++) {
-			for (int j = _firstJ; j < _lastJ; j++) {
-				Instantiate (free,new Vector3 (i,j,-1),Quaternion.identity);
+        bool ok = true;
+        int i = _firstI;
+		while(ok && i< _lastI) {
+            int j = _firstJ;
+			while (ok && j < _lastJ) {
+                if(occupied[i-aux.initialI,j-aux.initialJ])
+                {
+				    
+                    ok = false;
+                }
+                j++;
 			}
+            i++;
 		}
+        if (!ok)
+        {
+            roomController = null;
+        }else
+        {
+            roomController = new GameObject();
+            roomController.name = "Room\tInit: " + _firstI;
+            for (int x = _firstI; x < _lastI; x++)
+            {
+                for (int y = _firstJ; y < _lastJ; y++)
+                {
+                    GameObject newRoom = Instantiate(elements[Random.Range(0, elements.Length)], new Vector3(i, j, 0), Quaternion.identity);
+                    newRoom.transform.SetParent(roomController.transform);
+                }
+            }
+           
+            for (int x = _firstI-1; x < _lastI+1; x++)
+            {
+                for(int y = _firstJ-1; y < _lastJ+1; y++)
+                {
+                    occupied[x-aux.initialI,y-aux.initialJ] = true;
+                }
+            }
+        }
 	}
+
+    public int getFirstI()
+    {
+        return _firstI;
+    }
+
+    public int getLastI()
+    {
+        return _lastI;
+    }
+
+    public int getFirstJ()
+    {
+        return _firstJ;
+    }
+
+    public int getLastJ()
+    {
+        return _lastJ;
+    }
+
+    public GameObject getRoomController()
+    {
+        return roomController;
+    }
 }
